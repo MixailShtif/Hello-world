38da873a6df3381e9e7f5fdd5857636d8df3c3c0
MIKHAIL SHTIF <mixa_shtif@mail.ru>
2023-06-21 19:19:59 +0500

    SAVE

---
+++ C# HelloConsole/Task #0/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();//отчистка экрана
+Console.WriteLine("Введите число => ");//Вывод приглашения
+string inputedValue = Console.ReadLine();//ввод пользователя (строки)
+int number = Convert.ToInt32(inputedValue);//Преобразует строку в целое число
+Console.WriteLine("Вы ввели число " + number);
+
+int square = number * number;// вычесление квадрата
+Console.WriteLine("квадрат числа " + square);//вывод результата
+
