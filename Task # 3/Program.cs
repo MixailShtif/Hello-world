88fecac130dd57105d037ef97698563c47291f84
MIKHAIL SHTIF <mixa_shtif@mail.ru>
2023-06-23 17:43:22 +0500

    save

---
+++ Task # 3/Program.cs
@@ -0,0 +1,6 @@
+﻿// Выдавать день недели по заданному числу 3 среда, 5 пятница
+Console.WriteLine("Введите число");
+string Day = Console.ReadLine();
+int DayNumber = Convert.ToInt32(Day);
+string [] weeks = {"Monday", "Tuesday", "Wendsday", "Thursday", "Friday","Saturday", "Sunday"};
+Console.WriteLine(weeks [DayNumber -1]);
